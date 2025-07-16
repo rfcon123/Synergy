72663dba3abad3c55b833a8a713c2701fc775aea
rfcon123 <experiment@rfconnector.in>
2025-07-16 16:27:01 +0530

    12

--- src/pages/ProductCatalogue.jsx
+++ src/pages/ProductCatalogue.jsx
@@ -81,7 +81,7 @@ const catalogueLinks = [
   { name: "RF Test Measurement Equipment Stock", link: "https://www.rfconnector.in/Product/Product_Cataloge/RF_Test_Measurement_Equipment_Stock.pdf" },
   { name: "Equipment Photo And Price", link: "https://rfconnector.in/Product/EQUIPMENT%20PHOTO%20AND%20PRICE.pdf" },
   { name: "ST High Frequency Antenna", link: "https://rfconnector.in/Product/ST_HIGH%20_FREQUENCY_ANTENNA.pdf" },
-    { name: "8 band Panel Antenna", link: "https://panaceaticsynergy.com/Datasheets/8-BAND_ATENNA_PATCH-PANEL.pdf" },
+    { name: "8 band Panel Antenna", link: "/Datasheets/8-BAND_ATENNA_PATCH-PANEL.pdf" },
 
 ];
 
