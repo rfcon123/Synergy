f0c1727169c27a0a94c55785924de60c6b25bb9c
rfcon123 <experiment@rfconnector.in>
2025-08-13 18:04:37 +0530

    update

--- src/pages/Productshowroom.jsx
+++ src/pages/Productshowroom.jsx
@@ -197,6 +197,7 @@ import LISTENTALK from '../assets/components/LISTEN & TALK.jpeg';
 import POWERSUPPLYSM3540 from '../assets/components/POWER SUPPLY SM-3540.jpeg';
 import GENERATOR33210A from '../assets/components/GENERATOR 33210A.jpeg';
 import PowerboxLVM210290 from '../assets/components/Powerbox LVM 210.290.jpg';
+import NRVZ53POWERMETERSENSOR from '../assets/components/NRV Z53 POWER METER SENSOR.png';
 
 
 
@@ -249,6 +250,7 @@ const productsData = [
   { name: "Power Supply SM-3540", slug: "power-supply-sm-3540", image: POWERSUPPLYSM3540 },
   { name: "Generator 33210A", slug: "generator-33210a", image: GENERATOR33210A },
   { name: "Powerbox LVM 210.290", slug: "powerbox-lvm-210-290", image: PowerboxLVM210290 },
+  { name: "NRV Z53 Power Meter Sensor", slug: "nrv-z53-power-meter-sensor", image: NRVZ53POWERMETERSENSOR },
   
 
 
