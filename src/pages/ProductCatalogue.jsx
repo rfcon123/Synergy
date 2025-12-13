3ebfd85c4a642b86467854a182d400f42e70406e
rfcon123 <experiment@rfconnector.in>
2025-12-13 13:36:22 +0530

    link update

--- src/pages/ProductCatalogue.jsx
+++ src/pages/ProductCatalogue.jsx
@@ -197,6 +197,8 @@ const catalogueLinks = [
   { name: "RF Test Measurement Equipment Stock", link: "https://www.rfconnector.in/Product/Product_Cataloge/RF_Test_Measurement_Equipment_Stock.pdf" },
   { name: "Equipment Photo And Price", link: "https://rfconnector.in/Product/EQUIPMENT%20PHOTO%20AND%20PRICE.pdf" },
   { name: "ST High Frequency Antenna", link: "https://rfconnector.in/Product/ST_HIGH%20_FREQUENCY_ANTENNA.pdf" },
+  { name: "MAIN PRODUCT LINK", link: "https://rfconnector.in/Product/MAIN_PRODUCT_LINK/" },
+
 
 
 
