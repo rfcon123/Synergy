c153f4e6480789dbbb771ea8407593e37fc98dc6
rfcon123 <experiment@rfconnector.in>
2026-01-09 11:59:57 +0530

    done

--- src/pages/Productshowroom.jsx
+++ src/pages/Productshowroom.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-refresh/only-export-components */
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from "react-helmet-async";
