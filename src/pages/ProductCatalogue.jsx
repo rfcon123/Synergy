a6c102a70fba1f0205d660efd0ae54adb2c8302a
rfcon123 <experiment@rfconnector.in>
2026-02-23 18:15:32 +0530

    update

--- src/pages/ProductCatalogue.jsx
+++ src/pages/ProductCatalogue.jsx
@@ -111,8 +111,9 @@ const catalogueLinks = [
   { name: "Specs 25W 8G Attenuator", link: "/pdfs/specs 25w 8G attenuator.pdf" },
   { name: "SST-AT-6G-100W-NF-NM", link: "/pdfs/ST-AT-6G-100W-NF-NM.pdf" },
   { name: "0.5-30GHz Broadband Horn Antenna", link: "/pdfs/0.5-30GHz Broadband Horn Antenna.pdf" },
-  { name: "JAMMER MODULAR", link: "/pdfs/JAMMER_MODULAR.pdf" },
-  { name: "RTP 2RTP 40GHz", link: "/pdfs/RTP 40GHz.pdf" },
+  { name: "Corrugated", link: "/pdfs/Corrugated.pdf" },
+
+
 
 
 
