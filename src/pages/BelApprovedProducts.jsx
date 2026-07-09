83b196b22d846e9647451c00c4bff6e7a36e9430
synergy telecome <rfconnectorhouse.com>
2026-07-09 13:48:10 +0530

    update

--- src/pages/BelApprovedProducts.jsx
+++ src/pages/BelApprovedProducts.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ProductCatalogue from './ProductCatalogue';
 
 const belApprovedProducts = [
-  { name: 'BEL Approved Product 1', link: '/pdfs/ST- Antenna Catalog.pdf' },
-  { name: 'BEL Approved Product 2', link: '/pdfs/ST-Technology  product catalog6.9.pdf' },
-  { name: 'BEL Approved Product 3', link: '/pdfs/HIGH PERFORMANCE DISH ANTENNA.pdf' },
-  { name: 'BEL Approved Product 4', link: '/pdfs/Power supply.pdf' },
-  { name: 'BEL Approved Product 5', link: '/pdfs/RF COAXIAL.pdf' },
-  { name: 'BEL Approved Product 6', link: '/pdfs/5G INDOOR DIRECTIONAL PANEL ANTENNA.pdf' },
+  { name: 'series approved as on 08 JAN 2026', link: '/BEL Approved Products/STK- series approved as on 08 JAN 2026.pdf' },
+
+
+
+
+
+
+
 ];
 
 const BelApprovedProducts = () => (
